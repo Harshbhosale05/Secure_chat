a245c20de595831c9cce9f24c5ff55210b791bdf
Harshvardhan Bhosale <hbhosale2004@gmail.com>
2025-07-27 12:15:24 +0530

    login_sorted

--- securechat/fullstack-chat-app/frontend/src/lib/axios.js
+++ securechat/fullstack-chat-app/frontend/src/lib/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://secure-chat-1-540z.onrender.com";
 
 export const axiosInstance = axios.create({
-	baseURL: API_BASE_URL,
+	baseURL: `${API_BASE_URL}/api`,
 	withCredentials: true,
 });
